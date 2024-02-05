f656da543ac67ebd11f1bda16f969332ce018f99
JhayStark <jaamen5@gmail.com>
2024-02-05 08:37:08 +0000

    fix:suspend boundary

--- src/app/app/registration/page.tsx
+++ src/app/app/registration/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as z from 'zod';
-import React, { useMemo, Suspense } from 'react';
+import React, { useMemo } from 'react';
 import PersonalInformation from '@/components/registration/personal-information';
 import ResidentialInformation from '@/components/registration/residential-information';
 import {
@@ -105,102 +105,97 @@ const Page = () => {
   }
 
   return (
-    <Suspense>
-      <Card className=' bg-slate-50  max-w-[550px] mx-auto '>
-        <CardHeader className=''>
-          <CardTitle className='text-center text-xl relative'>
-            {query && (
-              <ArrowLeft
-                className='absolute cursor-pointer'
-                onClick={previousPage}
-              />
-            )}
-            {`${
-              registrationPages[parseInt(query ?? '0') - 1] ||
-              'Welcome To Academy'
-            } `}
-          </CardTitle>
+    <Card className=' bg-slate-50  max-w-[550px] mx-auto '>
+      <CardHeader className=''>
+        <CardTitle className='text-center text-xl relative'>
           {query && (
-            <CardDescription className='pb-5 text-gray-500 text-center'>
-              Fill out the form below to register your ward
-            </CardDescription>
-          )}
-          {query && (
-            <Progress
-              value={calculateProgress()}
-              className='h-1 bg-gray-700'
-              color='white'
+            <ArrowLeft
+              className='absolute cursor-pointer'
+              onClick={previousPage}
             />
           )}
-        </CardHeader>
+          {`${
+            registrationPages[parseInt(query ?? '0') - 1] ||
+            'Welcome To Academy'
+          } `}
+        </CardTitle>
+        {query && (
+          <CardDescription className='pb-5 text-gray-500 text-center'>
+            Fill out the form below to register your ward
+          </CardDescription>
+        )}
+        {query && (
+          <Progress
+            value={calculateProgress()}
+            className='h-1 bg-gray-700'
+            color='white'
+          />
+        )}
+      </CardHeader>
 
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className='space-y-6 p-1 overflow-y-auto  '
-            >
-              {!query && (
-                <div className='space-y-3'>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='space-y-6 p-1 overflow-y-auto  '
+          >
+            {!query && (
+              <div className='space-y-3'>
+                <p>
+                  <span className='italic'>Welcome to the pitch!</span> ⚽️
+                </p>
+                <div className='space-y-5 text-justify'>
                   <p>
-                    <span className='italic'>Welcome to the pitch!</span> ⚽️
+                    We&apos;re thrilled to have [ward&apos;s name] join our
+                    soccer academy at [academy name]! Get ready for a season of
+                    fun, skill-building, and teamwork as [ward&apos;s name]
+                    hones their footwork, builds confidence, and makes lasting
+                    memories with our passionate coaches and fellow future
+                    stars.
                   </p>
-                  <div className='space-y-5 text-justify'>
-                    <p>
-                      We&apos;re thrilled to have [ward&apos;s name] join our
-                      soccer academy at [academy name]! Get ready for a season
-                      of fun, skill-building, and teamwork as [ward&apos;s name]
-                      hones their footwork, builds confidence, and makes lasting
-                      memories with our passionate coaches and fellow future
-                      stars.
-                    </p>
-                    <p>
-                      Complete your registration and secure your spot on the
-                      team!
-                    </p>
-                    <p>
-                      Just click the button below to finalize the process and
-                      kick off this exciting journey:
-                    </p>
+                  <p>
+                    Complete your registration and secure your spot on the team!
+                  </p>
+                  <p>
+                    Just click the button below to finalize the process and kick
+                    off this exciting journey:
+                  </p>
 
-                    <p>
-                      We can&apos;t wait to see your little Messi or Ronaldo in
-                      action!
-                    </p>
-                    <p>Cheers,</p>
-                    <p>The [Academy name] Team</p>
-                    <p>
-                      P.S. Need any help? Contact us at [email address or phone
-                      number] - we&apos;re always happy to assist!
-                    </p>
-                  </div>
+                  <p>
+                    We can&apos;t wait to see your little Messi or Ronaldo in
+                    action!
+                  </p>
+                  <p>Cheers,</p>
+                  <p>The [Academy name] Team</p>
+                  <p>
+                    P.S. Need any help? Contact us at [email address or phone
+                    number] - we&apos;re always happy to assist!
+                  </p>
                 </div>
-              )}
-              {parseInt(query ?? '0') === 1 && (
-                <PersonalInformation form={form} />
-              )}
-              {parseInt(query ?? '0') === 2 && (
-                <ResidentialInformation form={form} />
-              )}
-              {parseInt(query ?? '0') === 3 && (
-                <ParentalInformation form={form} />
-              )}
-              {parseInt(query ?? '0') === 4 && (
-                <HealthInformation form={form} />
-              )}
-              {parseInt(query ?? '0') === 5 && <Education form={form} />}
-              {parseInt(query ?? '0') < registrationPages.length ? (
-                <Button className='w-full' type='button' onClick={nextPage}>
-                  Next
-                </Button>
-              ) : (
-                <Button className='w-full'>Register</Button>
-              )}
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </Suspense>
+              </div>
+            )}
+            {parseInt(query ?? '0') === 1 && (
+              <PersonalInformation form={form} />
+            )}
+            {parseInt(query ?? '0') === 2 && (
+              <ResidentialInformation form={form} />
+            )}
+            {parseInt(query ?? '0') === 3 && (
+              <ParentalInformation form={form} />
+            )}
+            {parseInt(query ?? '0') === 4 && <HealthInformation form={form} />}
+            {parseInt(query ?? '0') === 5 && <Education form={form} />}
+            {parseInt(query ?? '0') < registrationPages.length ? (
+              <Button className='w-full' type='button' onClick={nextPage}>
+                Next
+              </Button>
+            ) : (
+              <Button className='w-full'>Register</Button>
+            )}
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
 
